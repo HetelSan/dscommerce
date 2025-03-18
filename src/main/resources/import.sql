766be536be4e93236ac57de60373404430862613
Hetel Santos <hetel.santos@gmail.com>
2025-03-18 16:21:59 -0300

    first commit with Order

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '123456', '2001-07-25');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '123456', '1987-12-13');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Jack Black', 'jack@gmail.com', '977777777', '123456', '1997-05-30');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Mary Silver', 'mary@gmail.com', '977777777', '123456', '1996-09-06');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('John White', 'john@gmail.com', '977777777', '123456', '1998-08-27');
