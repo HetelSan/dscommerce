1ef681225aa5dd39daf076aaba79c0c962555d28
Hetel Santos <hetel.santos@gmail.com>
2025-03-21 16:20:51 -0300

    GET product by id

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,27 @@
 package com.devsuperior.dscommerce.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 	
-	@GetMapping
-	public String teste() {
-		return "Ola mundo!";
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping(value = "/{id}")
+	public ProductDTO findById(@PathVariable Long id) {
+		/* forma didatica */
+		// ProductDTO dto = productService.findById(id);
+		// return dto;
+		/* forma resumida */
+		return productService.findById(id);
 	}
 }
