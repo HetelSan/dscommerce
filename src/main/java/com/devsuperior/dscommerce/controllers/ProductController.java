0e6032c09dbe7c38095d28fcb40c7ecd410c4509
Hetel Santos <hetel.santos@gmail.com>
2025-03-21 21:10:26 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,10 @@ public class ProductController {
 		// return dto;
 		/* forma resumida */
 		return productService.findById(id);
+	}
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return productService.findAll(pageable);
 	}
 }
